33984cd76f1f9f463dd3b8e66d791eebdf133849
telegott <michael.mutz@posteo.de>
2022-01-28 00:14:52 +0100

    Add mockery experiment (#7)

---
+++ tests/testthat/test-process.R
@@ -0,0 +1,23 @@
+test_that("process produces output", {
+  actual <- process("doesnt-matter-in-this-example")
+  expect_true(is.numeric(actual))
+})
+
+test_that("process_all collects results and errors", {
+  process_mock <- function(input) {
+    if (input == "b") {
+      stop("could not calculate for input 'b'")
+    } else {
+      input
+    }
+  }
+
+  mockery::stub(process_all, "process", process_mock)
+
+  expected <- list(
+    results = list("a", "c"),
+    failures = list("could not calculate for input 'b'")
+  )
+  actual <- process_all(c("a", "b", "c"))
+  expect_equal(actual, expected)
+})
