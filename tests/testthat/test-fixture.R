ac8f4d8722f935d688d19bfe356338ddf5677690
michael <michael.mutz@posteo.de>
2022-03-03 22:02:23 +0100

    Add fixture example

--- tests/testthat/test-fixture.R
+++ tests/testthat/test-fixture.R
@@ -1,5 +1,5 @@
 test_that("fixtures can be read", {
-  path <- test_path(fs::path('fixtures', 'the-fixture.csv'))
+  path <- test_path(fs::path("fixtures", "the-fixture.csv"))
   expected <- data.frame(x = c(1, 3), y = c(2, 4))
   expect_equal(read.csv(path), expected)
 })
